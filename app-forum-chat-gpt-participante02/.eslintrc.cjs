896a1d6fc9d178392df97bbb24324e8fa93fc7e9
Mauricio Monteiro <mauricio.monteiro@ifmg.edu.br>
2023-12-01 11:30:20 -0300

    Analysis Quality Code

--- app-forum-chat-gpt-participante02/.eslintrc.cjs
+++ app-forum-chat-gpt-participante02/.eslintrc.cjs
@@ -4,18 +4,20 @@
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
   ],
   overrides: [
     {
-      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
-      extends: ['plugin:cypress/recommended']
-    }
+      files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
+      extends: ["plugin:cypress/recommended"],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+};
